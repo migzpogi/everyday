ce3f956112946fa1fc455bed15eff31cc3ba57be
migze <miguel_estrella@trendmicro.com>
2017-05-08 15:54:46 +0800

    Usage of __main__

---
+++ 02_05062017_main.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+	print __name__
+
+# Before executing the code, it will define a few special variables. For example, if the python interpreter is running that module (the source file) as the main program, it sets the special __name__ variable to have a value "__main__". If this file is being imported from another module, __name__ will be set to the module's name. One of the reasons for doing this is that sometimes you write a module (a .py file) where it can be executed directly. Alternatively, it can also be imported and used in another module. By doing the main check, you can have that code only execute when you want to run the module as a program and not have it execute when someone just wants to import your module and call your functions themselves.
